317a5a31cc4497131caefc9c4244be5672592965
Kanika <kanika.skyquestt@gmail.com>
2022-04-05 02:37:39 +0530

    Added main method

--- src/LockedMeMain.java
+++ src/LockedMeMain.java
@@ -4,15 +4,7 @@ public class LockedMeMain {
      
 	public static void main(String[] args) {
 		DisplayMenu.welcomeScreen();
-		DisplayMenu.displayMenu();
-		//DisplayMenu.displayFileMenu();
-		//FileOperations.createMainFolderIfNotPresent("main");
-		//FileOperations.display();
-		//FileOperations.ascendingOrder();
-		//FileAddOperations.addingFile();
-		//FileSearchOperation.searchingFile();
-		
-				
+		DisplayMenu.displayMenu();		
 	}
 
 }
